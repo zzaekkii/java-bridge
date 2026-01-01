35461bb5d7cff2d0a1edadb5926f6e9102f3e10a
zakie <realzzaekkii@gmail.com>
2026-01-01 22:05:45 +0900

    feat: 에러 메시지 출력 메서드 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,9 +4,15 @@ package bridge.view;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
     
     public void printGameStart() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
+
+    public void printBridgeSizeRequest() {
+        System.out.println("다리의 길이를 입력해주세요.");
     }
 
     /**
@@ -23,5 +29,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println(ERROR_MESSAGE_PREFIX + message);
     }
 }
